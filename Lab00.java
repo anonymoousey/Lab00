bf0e5bb6843b1dc99c613b43d53e76182552f59b
anonymoose <92691870+anonymoousey@users.noreply.github.com>
2024-10-07 19:03:47 -0400

    Add files via upload

---
+++ Lab00.java
@@ -0,0 +1,17 @@
+//Zachary, lab00
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+public class Lab00
+{
+   public static void main(String[] args)
+   {
+      Display.openWorld("maps/first.map");
+      Display.setSize(10,10);
+      Display.setSpeed(1);
+      
+      Robot karel = new Robot();
+      
+                                                                                                           
+   }
+}
